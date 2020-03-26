2f69890b43fd042a1016358c31973db3600a4b5a
saritha <sirideep.boddu@gmail.com>
2020-03-26 01:05:39 -0700

    t commit

--- Tests/specs/Leads/DestinationPageGetPricing.js
+++ Tests/specs/Leads/DestinationPageGetPricing.js
@@ -6,14 +6,8 @@ describe("Destination Page leads", function() {
 it("getpricing lead " , function () {
     destinationPage.open()
 
-browser.setupInterceptor();
-    
-    destinationPage.getpricinglead_Email.setValue('test@aplaceformom.com')
     destinationPage.getPricingButton.click()
-   /* browser.pause(2000);
-    var request = browser.getRequest(0);
-assert.equal(request.method, 'POST');
-assert.equal(request.response.headers['content-length'], '42'); */
+    destinationPage.getpricinglead_Email.setValue('test@aplaceformom.com')
     destinationPage.getpricinglead_Fullname.setValue('test')
     destinationPage.getpricinglead_Phone.setValue('5555555555')
     destinationPage.getpricinglead_submitbutton.click()
